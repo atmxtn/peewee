b2c1bcab0527432eb5c0fa9be8cd81b461b5d214
Cipeczek <oskarwojcik@proton.me>
2022-11-07 19:42:42 +0100

    Huge Update!
    Implemented most hallowen features, refactored code, fixed bugs. Playable pre pre-alpha build

--- birthday_clock.js
+++ birthday_clock.js
@@ -33,9 +33,9 @@ let x = setInterval(function () {
         $("#hepi-berzdej-timer").text("HEPI BERZDEJ STARA PIZIU!!!");
         $("#hepi-berzdej-timer").css('font-size', "10vh");
         $("#hepi-berzdej-timer").after("<p class='fnt' id='lol' style='font-size: 4vh'>(najslodsa i najpiekniejsza piziu, spełnienia wszyskiego. plynnego francais i mialnego life )</p>")
-        $("#lol").after('<iframe class="cizia" width="100%" height="100" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/942868204&color=%23ff5500&auto_play=true&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe>')
+        //$("#lol").after('<iframe class="cizia" width="100%" height="100" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/942868204&color=%23ff5500&auto_play=true&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe>')
         $('.cizia').after("<p id='sex' class='fnt' style='font-size: 2vh;'>(jebany autoplay nie dzialal na chromie a pewnie z tg korzystasz)</p>")
-        $('#sex').after('<div id="start-button" style="margin-top: 10px"><button id="start-button" style="color: red; background-color:#ffffff42; width: 20vw; height:8vh; font-size: 20px;  font-family: "Monsterrat", sans-serif; " onClick="gameInit()">Start the MaxiKing game</button></div>')
+        $('#lol').after('<div id="start-button" style="margin-top: 10px"><button id="start-button" style="color: red; background-color:#ffffff42; width: 20vw; height:8vh; font-size: 20px;  font-family: "Monsterrat", sans-serif; " onClick="gameInit()">Start the MaxiKing game</button></div>')
         $("#start-button").after("<button onClick='disableConfetti()'>disable confetti</button>")
 
 
